a70a474b6092532928b4d49743dff96ee2bdb057
Ousmane Ndiaye <ousmanend2211@gmail.com>
2024-03-13 14:04:39 +0100

    Ajoutez des fichiers projet.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+
+       [HttpGet]
+        public JsonResult GetAllCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                      new {id = 1, name = "Ousmane"},
+                      new {id = 2, name = "Fatou"},
+                }); 
+        }
+    }
+}
